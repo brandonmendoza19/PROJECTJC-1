d726b9bf74d8c23c171359ac4b65b55eba820e95
brandonmendoza19 <e.sda.brandon.mendoza@cali.edu.co>
2024-11-05 18:21:54 -0500

    Primer commit

---
+++ inicio.js
@@ -0,0 +1,13 @@
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const button = document.getElementById('button')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const data = {
+        username: username.ariaValueMax,
+        password: password.value
+    }
+
+console.log(data)
+})
